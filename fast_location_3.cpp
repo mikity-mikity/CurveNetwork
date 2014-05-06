c3c66473a0feb6a58c12a12b23f97584c0b2bd9d
mikity-mikity <mikity.kogekoge@gmail.com>
2014-05-07 08:26:07 +0900

    ofufu

--- fast_location_3.cpp
+++ fast_location_3.cpp
@@ -29,7 +29,8 @@ typedef Delaunay::Locate_type    Locate_type;
 using namespace std;
 int main()
 {
-	string filename="c:/out/cube.txt";
+	string NAME="test";
+	string filename="c:/out/"+NAME+".txt";
 	std::cout<<"start reading file"<<"["<<filename<<"]"<<endl;
 
 	ifstream ifs(filename);
@@ -72,11 +73,43 @@ int main()
 	}
 
 	//Generating points along branches.
-	std::vector</*std::pair<*/Point/*,unsigned>*/> exterior;
+	std::vector<Point> exterior;
 	std::vector<Point> interior;
 	int C=0;
 	int num=0;
 	double pi=boost::math::constants::pi<double>();
+	for(vector<R_br>::iterator itr=data.begin();itr!=data.end();itr++,C++)
+	{
+		double Radius;
+		br* _branch;
+		boost::tie(Radius,_branch)=*itr;
+		exterior.push_back(*(_branch->begin()));
+		for(br::iterator anitr=_branch->begin();anitr!=_branch->end()-1;anitr++)
+		{
+			Point P=*anitr;
+			Point Q=*(anitr+1);
+			Vector V=Q-P;
+			V=V/std::sqrt(V.squared_length());
+			Vector Z=Vector(0,0,1);
+			double t=V*Z;
+			if(std::fabs(t)>0.9)
+			{
+				Z=Vector(1,0,0);			
+			}
+			Vector W=CGAL::cross_product<K>(Z,V);
+			Vector T=CGAL::cross_product<K>(V,W);
+			//exterior.push_back(P);
+			for(int i=0;i<12;i++)
+			{
+				double theta=(double)i/12.*2.*pi;
+				Vector N=Radius*(W*std::cos(theta)+T*std::sin(theta));
+				Point D=Point(N.x()+0.5*(P.x()+Q.x()),N.y()+0.5*(P.y()+Q.y()),N.z()+0.5*(P.z()+Q.z()));
+				exterior.push_back(D);
+			}
+		}
+		exterior.push_back(*(_branch->end()-1));
+	}
+	
 	for(vector<R_br>::iterator itr=data.begin();itr!=data.end();itr++,C++)
 	{
 		double Radius;
@@ -89,57 +122,47 @@ int main()
 			Vector V=Q-P;
 			V=V/std::sqrt(V.squared_length());
 			Vector Z=Vector(0,0,1);
-			Vector W=CGAL::cross_product<K>(Z,V);
-			Vector T=CGAL::cross_product<K>(Z,W);
-			exterior.push_back(/*std::make_pair(*/P/*,++num)*/);
-			for(int i=0;i<8;i++)
+			double t=V*Z;
+			if(std::fabs(t)>0.9)
 			{
-				double theta=(double)i/8.*2.*pi;
-				Vector N=Radius*(W*std::cos(theta)+T*std::sin(theta));
-				Point D=Point(N.x()+0.5*(P.x()+Q.x()),N.y()+0.5*(P.y()+Q.y()),N.z()+0.5*(P.z()+Q.z()));
-				exterior.push_back(/*std::make_pair(*/D/*,++num)*/);
-				for(double scale=0.2;scale<1.0;scale+=0.2)
+				Z=Vector(1,0,0);			
+			}
+			Vector W=CGAL::cross_product<K>(Z,V);
+			Vector T=CGAL::cross_product<K>(V,W);
+			//exterior.push_back(P);
+			for(int i=0;i<12;i++)
+			{
+				double theta=(double)((double)i+0.5)/12.*2.*pi;
+				for(double scale=0.5;scale<1.0;scale+=0.05)
 				{
 					Vector N=scale*Radius*(W*std::cos(theta)+T*std::sin(theta));
-					Point D=Point(N.x()+(P.x()+Q.x())*0.5,N.y()+(P.y()+Q.y())*0.5,N.z()+(P.z()+Q.z())*0.5);
+					Point D=Point(N.x()+(P.x()),N.y()+(P.y()),N.z()+(P.z()));
 					interior.push_back(D);				
 				}
 			}
 		}
-		exterior.push_back(/*std::make_pair(*/*(_branch->end()-1)/*,++num)*/);
 	}
-	
 
-	//File write
-	/*filename="c:/out/cube.out";
-	cout<<"start writing file"<<"["<<filename<<"]"<<endl;
-
-	ofstream ofs(filename);
-	ofs<<exterior.size()<<endl;
-	for(std::vector<Point>::iterator itr=exterior.begin();itr!=exterior.end();itr++)
-	{
-		Point P=*itr;
-		ofs<<P.x()<<" "<<P.y()<<" "<<P.z()<<endl;
-	}
-	ofs.close();
-	*/
 	// building their Delaunay triangulation.
 	std::cout<<"start triangulation"<<endl;
 	
 	Delaunay T;
 	C2t3 ct(T);
-	
+	std::cout<<"exterior.size()"<<exterior.size()<<endl;
 	N=0;
 	int NN=(exterior.size()/20);
 	for(int i=0;i<20;i++)
 	{
-		std::vector</*std::pair<*/Point/*,unsigned>*/>::iterator be=exterior.begin()+exterior.size()*i/20;
-		std::vector</*std::pair<*/Point/*,unsigned>*/>::iterator en=exterior.begin()+exterior.size()*(i+1)/20-1;
+		std::vector<Point>::iterator be=exterior.begin()+exterior.size()*i/20;
+		std::vector<Point>::iterator en=exterior.begin()+exterior.size()*(i+1)/20;
 		T.insert(be,en);
 		std::cout<<"*";
 	}
 	std::cout<<endl;
 	std::cout<<"end triangulation"<<endl;
+	std::cout<<"T.number_of_vertices"<<T.number_of_vertices()<<endl;
+	std::cout<<"number_of_cells:"<<T.number_of_cells()<<endl;
+	std::cout<<"number_of_finite_cells:"<<T.number_of_finite_cells()<<endl;
 	Locate_type lt;
 	int li, lj;
 	N=0;
@@ -155,8 +178,6 @@ int main()
 	N=0;
 	NN=(interior.size()/20);
 	std::vector<int> cells;
-	cout<<"number_of_cells:"<<T.number_of_cells()<<endl;
-	cout<<"number_of_finite_cells:"<<T.number_of_finite_cells()<<endl;
 
 	for(int i=0;i<T.number_of_cells();i++)
 	{
@@ -182,8 +203,7 @@ int main()
 	cout<<"T.number_of_cells:"<<T.number_of_cells()<<endl;
 	cout<<"T.number_of_facets:"<<T.number_of_facets()<<endl;
 	cout<<"ct.number_of_facets:"<<ct.number_of_facets()<<endl;
-	std::cout << "Press Return To Exit...";
-	std::cin.get();
+
 	int max=*(std::max_element(cells.begin(),cells.end()));
 	for(int i=0;i<=max;i++)
 	{
@@ -191,36 +211,73 @@ int main()
 	}
 	
 	NN=T.number_of_facets()/20;
-	int S=T.number_of_facets()-1000000;
 	N=0;
-	for(Delaunay::Facet_iterator itr=T.facets_begin();itr!=T.facets_end()/*&&N<S*/;itr++,N++)
-	{
-		ct.add_to_complex(*itr);
-		//Delaunay::Facet f=ct.canonical_facet(T.cells_begin(),0);
-		//ct.change_in_complex_status<true,true>(itr->first,itr->second);
-		//cout<<N<<endl;
-		if(((int)N/NN)*NN==N)cout<<"*";
-	}
-	cout<<"complex created"<<endl;
-	cout<<"T.number_of_facets:"<<T.number_of_facets()<<endl;
-	cout<<"ct.number_of_facets:"<<ct.number_of_facets()<<endl;
-	cout<<endl;
-	/*for(Delaunay::Cell_iterator itr=T.cells_begin();itr!=T.cells_end();itr++)
+	for(Delaunay::Cell_iterator itr=T.cells_begin();itr!=T.cells_end();itr++,N++)
 	{
 		std::map<Delaunay::Cell_iterator,int>::iterator it_c=index.find(itr);
 		int d=it_c->second;
-		if(cells[d]<10)
+		if(cells[d]>=1)
 		{
-			//std::cout<<"remove a cell["<<d<<"]"<<endl;
-			//ct.remove_from_complex(itr,0);
-			//ct.remove_from_complex(itr,1);
-			//ct.remove_from_complex(itr,2);
-			//ct.remove_from_complex(itr,3);
-		}else
-		{
-			//std::cout<<"cell["<<d<<"]"<<"survived!"<<endl;
+			ct.add_to_complex(itr,0);
+			ct.add_to_complex(itr,1);
+			ct.add_to_complex(itr,2);
+			ct.add_to_complex(itr,3);
 		}
-	}*/
+		if(((int)N/NN)*NN==N)cout<<"*";
+	}
+	cout<<endl;
+	cout<<"complex created"<<endl;
+	cout<<"T.number_of_facets:"<<T.number_of_facets()<<endl;
+	cout<<"ct.number_of_facets:"<<ct.number_of_facets()<<endl;
+	std::map<C2t3::Cell_handle,int> space;
+	num=0;
+	for(C2t3::Facet_iterator itr=ct.facets_begin();itr!=ct.facets_end();itr++,num++)
+	{
+		space.insert(std::make_pair(itr->first,num));
+	}
+
+
+
+	//File write
+
+	
+	std::map<C2t3::Vertex_handle,int> vIndex;
+	filename="c:/out/"+NAME+".out";
+	string filename2="c:/out/"+NAME+"_S.out";
+	cout<<"start writing file"<<"["<<filename<<"]"<<endl;
+	ofstream ofs(filename);
+	ofstream ofs2(filename2);
+	N=0;
+	for(C2t3::Facet_iterator itr=ct.facets_begin();itr!=ct.facets_end();itr++)
+	{
+		C2t3::Facet opf=ct.opposite_facet(*itr);
+		//if(space.find(opf.first)!=space.end())
+		{
+			for(int i=0;i<4;i++)
+			{
+				if(i!=itr->second)
+				{
+					C2t3::Vertex_handle handle=itr->first->vertex(i);
+				
+					std::map<C2t3::Vertex_handle,int>::iterator pair=vIndex.find(handle);
+					if(pair==vIndex.end())
+					{
+						ofs<<handle->point().x()<<" , "<<handle->point().y()<<" , "<<handle->point().z()<<endl;
+						vIndex.insert(std::make_pair(handle,N));
+						ofs2<<N<<" ";
+						N++;
+					}else
+					{
+						ofs2<<pair->second<<" ";
+					}
+				}
+			}
+			ofs2<<endl;
+		}
+	}
+	ofs.close();
+	ofs2.close();
+	
 
 	std::cout << "Press Return To Exit...";
 	std::cin.get();
