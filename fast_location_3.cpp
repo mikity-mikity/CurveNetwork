6db2fe21ebc32727fa05bd959ec61545c637939b
mikity-mikity <mikity.kogekoge@gmail.com>
2014-05-09 17:37:29 +0900

    worked!

--- fast_location_3.cpp
+++ fast_location_3.cpp
@@ -27,7 +27,7 @@ typedef Delaunay::Locate_type    Locate_type;
 using namespace std;
 int main()
 {
-	string NAME="test";
+	string NAME="donut";
 	string filename="c:/out/"+NAME+".txt";
 	std::cout<<"start reading file"<<"["<<filename<<"]"<<endl;
 
@@ -73,7 +73,7 @@ int main()
 
 	//Generating points along branches.
 	std::vector<Point> exterior;
-	std::vector<std::pair<Point,double>> interior;
+	std::list<std::pair<Point,double>> interior;
 	int C=0;
 	int num=0;
 	double pi=boost::math::constants::pi<double>();
@@ -111,6 +111,10 @@ int main()
 	vector<R_br>::iterator itrA=data.begin();
 	vector<planes*>::iterator itrB=planeTree.begin();
 	std::cout<<"construct exterior"<<endl;
+
+    int NN=(data.size()/20);
+	if(NN<1)NN=1;
+	N=0;
 	while(itrA!=data.end())
 	{
 		double Radius;
@@ -146,22 +150,33 @@ int main()
 		exterior.push_back(*(_branch->end()-1));		
 		itrA++;
 		itrB++;
+		if(((int)N/NN)*NN==N)
+		{
+			std::cout<<"*";
+		}
+		N++;
 	}
+	std::cout<<endl;
 	itrA=data.begin();
 	itrB=planeTree.begin();
 	std::cout<<"construct interior"<<endl;
 	N=0;
+    NN=(data.size()/20);
+	if(NN<1)NN=1;
+
 	while(itrA!=data.end())
 	{
 		double Radius;
 		br* _branch;
 		planes* _planes=*itrB;
 		boost::tie(Radius,_branch)=*itrA;
-		double dx=Radius/5.;
+		double dx=Radius/10.;
 		br::iterator itrC=_branch->begin();
 		planes::iterator itrD=_planes->begin();
+		int N2=0;
 		while(itrC!=_branch->end()-1)
 		{
+			//std::cout<<"N="<<N<<","<<"N2="<<N2<<endl;
 			Vector V1=*itrD;
 			Vector V2=*(itrD+1);
 			Point P1=*itrC;
@@ -176,7 +191,7 @@ int main()
 			{
 				DX=dx/L;
 			}
-			for(double s=0.1;s<1.0;s+=DX)
+			for(double s=DX/2.;s<1.0;s+=DX)
 			{
 				Vector V=V1*s+V2*(1.-s);
 				Vector Z=Vector(0,0,1);
@@ -190,33 +205,40 @@ int main()
 				W=W/std::sqrt(W.squared_length());
 				T=T/std::sqrt(T.squared_length());
 				Point P(P1.x()*s+P2.x()*(1.-s),P1.y()*s+P2.y()*(1.-s),P1.z()*s+P2.z()*(1.-s));
-				for(double u=-1;u<=1;u+=0.2)
+				for(double u=-1;u<=1;u+=0.1)
 				{
-					for(double v=-1;v<=1;v+=0.2)
+					for(double v=-1;v<=1;v+=0.1)
 					{
 						if(u*u+v*v<=1)
 						{
-							interior.push_back(std::make_pair(P+Radius*W*u+Radius*T*v,DX*L*0.2*Radius*0.2*Radius));
+							Point PP=P+Radius*W*u+Radius*T*v;
+							interior.push_back(std::make_pair(PP,DX*L*0.1*Radius*0.1*Radius));
 						}
 					}
 				}
 			}
 			itrC++;
 			itrD++;
+			N2++;
 		}
 		itrA++;
 		itrB++;
+		if(((int)N/NN)*NN==N)
+		{
+			std::cout<<"*";
+		}
 		N++;
 	}
-	
-	// building their Delaunay tria*/ngulation.
+	std::cout<<endl;
+	// building their Delaunay triangulation.
 	std::cout<<"start triangulation"<<endl;
 	
 	Delaunay T;
-	//C2t3 ct(T);
 	std::cout<<"exterior.size()"<<exterior.size()<<endl;
+	std::cout<<"interior.size()"<<interior.size()<<endl;
 	N=0;
-	int NN=(exterior.size()/20);
+	NN=(exterior.size()/20);
+	if(NN<1)NN=1;
 	for(int i=0;i<20;i++)
 	{
 		std::vector<Point>::iterator be=exterior.begin()+exterior.size()*i/20;
@@ -226,19 +248,73 @@ int main()
 	}
 	std::cout<<endl;
 	std::cout<<"end triangulation"<<endl;
-	std::cout<<"T.number_of_vertices"<<T.number_of_vertices()<<endl;
-	std::cout<<"number_of_cells:"<<T.number_of_cells()<<endl;
+	std::cout<<"T.number_of_vertices:"<<T.number_of_vertices()<<endl;
 	std::cout<<"number_of_finite_cells:"<<T.number_of_finite_cells()<<endl;
-	Locate_type lt;
-	int li, lj;
 	N=0;
 	std::map<Delaunay::Cell_handle,int> index;
-	std::map<Delaunay::Cell_handle,double> volume;
+	//std::map<Delaunay::Cell_handle,double> volume;
+
+	double* cells=new double[T.number_of_finite_cells()];
+	//double* volume=new double[T.number_of_finite_cells()];
+	double* density=new double[T.number_of_finite_cells()];
+	//std::map<Delaunay::Cell_handle,double> density;
 	std::cout<<"start cell search"<<endl;
+	std::cout<<"create index"<<endl;
 
 	for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
 	{
 		index.insert(pair<const Delaunay::Finite_cells_iterator,int>(itr,N));
+	}
+
+	
+	Cell_handle before;
+	Cell_handle c;
+	std::cout<<"initialize cells"<<endl;
+	int S=T.number_of_finite_cells();
+	double* ptr1=cells;
+	double* ptr2=density;
+	for(int i=0;i<S;i++)
+	{
+		*ptr1=0;
+		*ptr2=0;
+		ptr1++;
+		ptr2++;
+	}
+	Locate_type lt;
+	int li, lj;
+	N=0;
+	std::cout<<"cell size"<<endl;
+	NN=(interior.size()/20);
+	if(NN<1)NN=1;
+	std::cout<<"start cell locate"<<endl;
+	int S1=0,S2=0;
+	for(list<std::pair<Point,double>>::iterator itr=interior.begin();itr!=interior.end();itr++,N++)
+	{
+		Point P=itr->first;
+		if(N==0)
+			c = T.locate(P, lt,li,lj);
+		else
+			c=T.locate(P,lt,li,lj,before);
+		if(lt==Locate_type::CELL)
+		{
+				std::map<Delaunay::Cell_handle,int>::iterator it_c=index.find(c);
+				int d=it_c->second;
+				cells[d]+=itr->second;
+				before=c;
+				S1++;
+		}
+		if(((int)N/NN)*NN==N)
+		{
+			std::cout<<"*";
+		}
+
+	}
+	std::cout<<endl;
+	std::cout<<"normal="<<S1<<","<<"error="<<S2<<endl;
+	std::cout<<"end cell search"<<endl;
+	N=0;
+	for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
+	{
 		Point PA=itr->vertex(0)->point();
 		Point PB=itr->vertex(1)->point();
 		Point PC=itr->vertex(2)->point();
@@ -247,72 +323,40 @@ int main()
 		Vector H=PD-PB;
 		Vector I=PD-PC;
 		double V=std::fabs(CGAL::cross_product(G,H)*I)/6.;
-		volume.insert(pair<const Delaunay::Finite_cells_iterator,double>(itr,V));
+		density[N]=cells[N]/V;
 	}
-	Cell_handle before=Cell_handle();
-	Cell_handle c=Cell_handle();
-	N=0;
-	NN=(interior.size()/20);
-	std::vector<double> cells;
 
-	for(int i=0;i<T.number_of_cells();i++)
-	{
-		cells.push_back(0);
-	}
-	for(vector<std::pair<Point,double>>::iterator itr=interior.begin();itr!=interior.end();itr++,N++)
-	{
-		if(N==0)
-			c = T.locate(itr->first, lt,li,lj);
-		else
-			c=T.locate(itr->first,lt,li,lj,before);
-		std::map<Delaunay::Cell_handle,int>::iterator it_c=index.find(c);
-		if(c>=T.finite_cells_begin()&&c<T.finite_cells_end())
-		{
-			int d=it_c->second;
-			cells[d]+=itr->second;
-			before=c;
-		}
-		if(((int)N/NN)*NN==N)
-		{
-			std::cout<<"*";
-		}
-	}
-	std::cout<<endl;
-	std::cout<<"end cell search"<<endl;
-	std::cout<<"T.number_of_cells:"<<T.number_of_cells()<<endl;
-	std::cout<<"T.number_of_facets:"<<T.number_of_facets()<<endl;
+	std::cout<<"T.number_of_finite_cells:"<<T.number_of_finite_cells()<<endl;
+	std::cout<<"T.number_of_finite_facets:"<<T.number_of_finite_facets()<<endl;
 
-	//int max=*(std::max_element(cells.begin(),cells.end()));
-	/*for(int i=0;i<=max;i++)
-	{
-		int n=std::count(cells.begin(),cells.end(),i);
-		if(n!=0)
-		std::cout<<i<<":"<<n<<endl;
-	}*/
-	/*Ž©‘OŽÀ‘•‚É‚·‚é*/
 	NN=T.number_of_finite_facets()/20;
+	if(NN<1)NN=1;
 	N=0;
 	std::vector<Delaunay::Facet> facet_list;
 	for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
 	{
-		std::map<Delaunay::Cell_handle,int>::iterator it_c=index.find(itr);
-		double V=volume.find(itr)->second;
-		double D=0.5;
+		double V=density[N];
+		double D=0.2;
 		for(int i=0;i<4;i++)
 		{
 			Delaunay::Cell_handle _neighbor=itr->neighbor(i);
+			int c=index.find(_neighbor)->second;
+			double Vi=density[c];
 			std::map<Delaunay::Cell_handle,int>::iterator it_N=index.find(_neighbor);
+			/*if(V>D)
+			{
+				std::cout<<V<<endl;
+				facet_list.push_back(Delaunay::Facet(itr,i));
+			}*/
 			if(it_N==index.end())
 			{
-				//if(cells[it_c->second]>=V*D)
+				if(V>D)
 					facet_list.push_back(Delaunay::Facet(itr,i));
 			}else
 			{
-				/*double V2=volume.find(_neighbor)->second;
-				if(cells[it_c->second]>=V*D&&cells[it_N->second]<V2*D)
-				{
+				if(V>D&&Vi<D)
 					facet_list.push_back(Delaunay::Facet(itr,i));
-				}*/
+			
 			}
 		}
 		if(((int)N/NN)*NN==N)std::cout<<"*";
@@ -321,7 +365,7 @@ int main()
 	std::cout<<"complex created"<<endl;
 	std::cout<<"T.number_of_finite_facets:"<<T.number_of_finite_facets()<<endl;
 
-
+	
 	//File write
 
 	
@@ -329,13 +373,28 @@ int main()
 	filename="c:/out/"+NAME+".out";
 	string filename2="c:/out/"+NAME+"_S.out";
 	string filename3="c:/out/"+NAME+"_F.out";
+	//string filename4="c:/out/"+NAME+"_D.out";
 	std::cout<<"start writing file"<<"["<<filename<<"]"<<endl;
 	ofstream ofs(filename);
 	ofstream ofs2(filename2);
 	ofstream ofs3(filename3);
-	for(vector<std::pair<Point,double>>::iterator itr=interior.begin();itr!=interior.end();itr++)
+	/*ofstream ofs4(filename4);
+	for(list<std::pair<Point,double>>::iterator itr=interior.begin();itr!=interior.end();itr++)
 	{
-		ofs3<<(itr->first).x()<<" , "<<(itr->first).y()<<" , "<<(itr->first).z()<<endl;
+		ofs4<<(itr->first).x()<<" , "<<(itr->first).y()<<" , "<<(itr->first).z()<<endl;
+	}*/
+	N=0;
+	for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
+	{
+		Point PA=itr->vertex(0)->point();
+		Point PB=itr->vertex(1)->point();
+		Point PC=itr->vertex(2)->point();
+		Point PD=itr->vertex(3)->point();
+		double x=(PA.x()+PB.x()+PC.x()+PD.x())/4.;
+		double y=(PA.y()+PB.y()+PC.y()+PD.y())/4.;
+		double z=(PA.z()+PB.z()+PC.z()+PD.z())/4.;
+		Point center(x,y,z);
+		ofs3<<center.x()<<" , "<<center.y()<<" , "<<center.z()<<" , "<<density[N]<<endl;
 	}
 	N=0;
 	for(vector<Delaunay::Facet>::iterator itr=facet_list.begin();itr!=facet_list.end();itr++)
@@ -364,12 +423,12 @@ int main()
 	ofs.close();
 	ofs2.close();
 	ofs3.close();
+	//ofs4.close();
 	
-
+	
 	std::cout << "Press Return To Exit...";
 	std::cin.get();
-	// performing nearest vertex queries to a series of random points,
-	// which is a case where the Fast_location policy is beneficial.
+
 	//release memory
 	for(vector<R_br>::iterator itr=data.begin();itr!=data.end();itr++)
 	{
@@ -381,7 +440,9 @@ int main()
 	{
 		delete(*itr);
 	}
-	//delete(cells);
+	delete(cells);
+	//delete(volume);
+	delete(density);
 
 
 
