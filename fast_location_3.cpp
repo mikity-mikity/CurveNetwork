0a3aaeb87202b1598e632355e5251072ab1ed047
mikity-mikity <mikity.kogekoge@gmail.com>
2014-05-14 23:37:17 +0900

    ddd

--- fast_location_3.cpp
+++ fast_location_3.cpp
@@ -12,9 +12,10 @@
 #include <boost/algorithm/string.hpp>
 #include<algorithm>	
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned,K>    Vb;
-typedef CGAL::Triangulation_data_structure_3<Vb>                    Tds;
-typedef CGAL::Delaunay_triangulation_3<K, Tds,CGAL::Fast_location> Delaunay;
+//typedef CGAL::Triangulation_3<K>      Delaunay;
+//typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned,K>    Vb;
+//typedef CGAL::Triangulation_data_structure_3<Vb>                    Tds;
+typedef CGAL::Delaunay_triangulation_3<K,CGAL::Fast_location> Delaunay;
 typedef Delaunay::Point Point;
 typedef CGAL::Vector_3<K> Vector;
 typedef std::vector<Point> br;
@@ -34,7 +35,7 @@ using namespace std;
 int main()
 {
 	double pi=boost::math::constants::pi<double>();
-	string NAME="test";
+	string NAME="test2";
 	string filename="c:/out/"+NAME+".txt";
 	std::cout<<"start reading file"<<"["<<filename<<"]"<<endl;
 
@@ -200,9 +201,17 @@ int main()
 		eclipses::iterator itrD=_eclipses->begin();
 		Vector beforeX(0,0,0);
 		Vector beforeY(0,0,0);
+		//Division number along diameter
+		int RDIV=12;
+		if(Radius*2.*pi/12.<baseRes)
+		{
+			RDIV=12;
+		}else{
+			RDIV=(int)(Radius*2.*pi/baseRes);
+		}
 		while(itrC!=_branch->end()-1)
 		{
-			exterior.push_back(*itrC);
+			//exterior.push_back(*itrC);
 			//Division number along line
 			Point P=*itrC;
 			Point Q=*(itrC+1);
@@ -215,14 +224,6 @@ int main()
 				DIV=1;
 			}else{
 				DIV=(int)(Length/baseRes)+1;
-			}
-			//Division number along diameter
-			int RDIV=12;
-			if(Radius*2.*pi/12.<baseRes)
-			{
-				RDIV=12;
-			}else{
-				RDIV=(int)(Radius*2.*pi/baseRes);
 			}
 			//if in the first run, before is igonored
 			//from the second run, before is used
@@ -261,41 +262,49 @@ int main()
 				beforeY=CGAL::cross_product(V,beforeX);
 
 			}
-			for(int i=0;i<RDIV;i++)
+			for(int ss=0;ss<=DIV;ss++)
 			{
-				double theta=(double)i/RDIV*2.*pi;
-
-				Vector B=Radius*(beforeX*std::cos(theta)+beforeY*std::sin(theta));
-
-				//Project N
-				double c1=-(B*itrD->Normal)/(V*itrD->Normal);
-				double c2=-(B*(itrD+1)->Normal)/(V*(itrD+1)->Normal);
-				Vector tmp1=B+c1*V;
-				Vector tmp2=B+c2*V;
-				Point D1=(*itrC)+tmp1;
-				Point D2=(*(itrC+1))+tmp2;
-				if(itrC==_branch->end()-2)
+				double s=((double)ss)/((double)DIV);
+				exterior.push_back(Point((*itrC).x()*(1-s)+(*(itrC+1)).x()*s,(*itrC).y()*(1-s)+(*(itrC+1)).y()*s,(*itrC).z()*(1-s)+(*(itrC+1)).z()*s));
+				for(int i=0;i<RDIV;i++)
 				{
-					for(int ss=0;ss<=DIV;ss++)
+					double theta=(double)i/RDIV*2.*pi;
+
+					Vector BI=0.8*Radius*(beforeX*std::cos(theta)+beforeY*std::sin(theta));
+					Vector BE=1.05*Radius*(beforeX*std::cos(theta)+beforeY*std::sin(theta));
+
+					//Project N
+					double cI1=-(BI*itrD->Normal)/(V*itrD->Normal);
+					double cI2=-(BI*(itrD+1)->Normal)/(V*(itrD+1)->Normal);
+					double cE1=-(BE*itrD->Normal)/(V*itrD->Normal);
+					double cE2=-(BE*(itrD+1)->Normal)/(V*(itrD+1)->Normal);
+					Vector tmpI1=BI+cI1*V;
+					Vector tmpI2=BI+cI2*V;
+					Vector tmpE1=BE+cE1*V;
+					Vector tmpE2=BE+cE2*V;
+					Point DI1=(*itrC)+tmpI1;
+					Point DI2=(*(itrC+1))+tmpI2;
+					Point DE1=(*itrC)+tmpE1;
+					Point DE2=(*(itrC+1))+tmpE2;
+					if(itrC==_branch->end()-2)
 					{
-						double s=ss/DIV;
-						Point D(D2.x()*s+D1.x()*(1-s),D2.y()*s+D1.y()*(1-s),D2.z()*s+D1.z()*(1-s));
-						exterior.push_back(D);
-					}
-				}else
-				{
-					for(int ss=0;ss<DIV;ss++)
+						Point DI(DI2.x()*s+DI1.x()*(1-s),DI2.y()*s+DI1.y()*(1-s),DI2.z()*s+DI1.z()*(1-s));
+						Point DE(DE2.x()*s+DE1.x()*(1-s),DE2.y()*s+DE1.y()*(1-s),DE2.z()*s+DE1.z()*(1-s));
+						exterior.push_back(DI);
+						exterior.push_back(DE);
+					}else
 					{
-						double s=((double)ss)/((double)DIV);
-						Point D(D2.x()*s+D1.x()*(1-s),D2.y()*s+D1.y()*(1-s),D2.z()*s+D1.z()*(1-s));
-						exterior.push_back(D);
+						Point DI(DI2.x()*s+DI1.x()*(1-s),DI2.y()*s+DI1.y()*(1-s),DI2.z()*s+DI1.z()*(1-s));
+						Point DE(DE2.x()*s+DE1.x()*(1-s),DE2.y()*s+DE1.y()*(1-s),DE2.z()*s+DE1.z()*(1-s));
+						exterior.push_back(DI);
+						exterior.push_back(DE);
 					}
 				}
 			}
 			itrC++;
 			itrD++;
 		}
-		exterior.push_back(*(_branch->end()-1));
+		//exterior.push_back(*(_branch->end()-1));
 
 		itrA++;
 		itrB++;
@@ -306,87 +315,11 @@ int main()
 		N++;
 	}
 	std::cout<<endl;
-	/*
-	itrA=data.begin();
-	itrB=eclipseTree.begin();
-	std::cout<<"construct interior"<<endl;
-	N=0;
-    NN=(data.size()/20);
-	if(NN<1)NN=1;
-
-	while(itrA!=data.end())
-	{
-		double Radius;
-		br* _branch;
-		planes* _planes=*itrB;
-		boost::tie(Radius,_branch)=*itrA;
-		double dx=Radius/10.;
-		br::iterator itrC=_branch->begin();
-		planes::iterator itrD=_planes->begin();
-		int N2=0;
-		while(itrC!=_branch->end()-1)
-		{
-			//std::cout<<"N="<<N<<","<<"N2="<<N2<<endl;
-			Vector V1=*itrD;
-			Vector V2=*(itrD+1);
-			Point P1=*itrC;
-			Point P2=*(itrC+1);
-			//•ªŠ„”Œˆ‚ß‚é
-			double L=std::sqrt((P1-P2).squared_length());
-			double DX=0.2;
-			if(L<dx*5)
-			{
-				DX=0.2;
-			}else
-			{
-				DX=dx/L;
-			}
-			for(double s=DX/2.;s<1.0;s+=DX)
-			{
-				Vector V=V1*s+V2*(1.-s);
-				Vector Z=Vector(0,0,1);
-				double t=V*Z;
-				if(std::fabs(t)>0.9)
-				{
-					Z=Vector(1,0,0);			
-				}
-				Vector W=CGAL::cross_product<K>(Z,V);
-				Vector T=CGAL::cross_product<K>(V,W);
-				W=W/std::sqrt(W.squared_length());
-				T=T/std::sqrt(T.squared_length());
-				Point P(P1.x()*s+P2.x()*(1.-s),P1.y()*s+P2.y()*(1.-s),P1.z()*s+P2.z()*(1.-s));
-				for(double u=-1;u<=1;u+=0.1)
-				{
-					for(double v=-1;v<=1;v+=0.1)
-					{
-						if(u*u+v*v<=1)
-						{
-							Point PP=P+Radius*W*u+Radius*T*v;
-							interior.push_back(std::make_pair(PP,DX*L*0.1*Radius*0.1*Radius));
-						}
-					}
-				}
-			}
-			itrC++;
-			itrD++;
-			N2++;
-		}
-		itrA++;
-		itrB++;
-		if(((int)N/NN)*NN==N)
-		{
-			std::cout<<"*";
-		}
-		N++;
-	}
-	std::cout<<endl;
-	*/
 	// building their Delaunay triangulation.
 	std::cout<<"start triangulation"<<endl;
 	
 	Delaunay T;
 	std::cout<<"exterior.size()"<<exterior.size()<<endl;
-	std::cout<<"interior.size()"<<interior.size()<<endl;
 	N=0;
 	NN=(exterior.size()/20);
 	if(NN<1)NN=1;
@@ -399,13 +332,14 @@ int main()
 	}
 	std::cout<<endl;
 	std::cout<<"end triangulation"<<endl;
+	std::cout<<"isValid"<<T.is_valid()<<endl;
 	std::cout<<"T.number_of_vertices:"<<T.number_of_vertices()<<endl;
 	std::cout<<"number_of_finite_cells:"<<T.number_of_finite_cells()<<endl;
 	N=0;
 	std::map<Delaunay::Cell_handle,int> index;
 	//std::map<Delaunay::Cell_handle,double> volume;
 
-	double* cells=new double[T.number_of_finite_cells()];
+	int* cells=new int[T.number_of_finite_cells()];
 	//double* volume=new double[T.number_of_finite_cells()];
 	double* density=new double[T.number_of_finite_cells()];
 	//std::map<Delaunay::Cell_handle,double> density;
@@ -422,7 +356,7 @@ int main()
 	Cell_handle c;
 	std::cout<<"initialize cells"<<endl;
 	int S=T.number_of_finite_cells();
-	double* ptr1=cells;
+	int* ptr1=cells;
 	double* ptr2=density;
 	for(int i=0;i<S;i++)
 	{
@@ -433,39 +367,262 @@ int main()
 	}
 	Locate_type lt;
 	int li, lj;
-	N=0;
-	std::cout<<"cell size"<<endl;
-	NN=(interior.size()/20);
-	if(NN<1)NN=1;
 	
-	/*std::cout<<"start cell locate"<<endl;
-	int S1=0,S2=0;
-	for(list<std::pair<Point,double>>::iterator itr=interior.begin();itr!=interior.end();itr++,N++)
+	std::cout<<"start cell locate"<<endl;
+	
+
+	N=0;
+	itrA=data.begin();
+	itrB=eclipseTree.begin();
+	while(itrA!=data.end())
 	{
-		Point P=itr->first;
-		if(N==0)
-			c = T.locate(P, lt,li,lj);
-		else
-			c=T.locate(P,lt,li,lj,before);
-		if(lt==Locate_type::CELL)
+		int YDIV=8;
+		double Radius;
+		br* _branch;
+		eclipses* _eclipses=*itrB;
+		boost::tie(Radius,_branch)=*itrA;
+		br::iterator itrC=_branch->begin();
+		eclipses::iterator itrD=_eclipses->begin();
+		Vector beforeX(0,0,0);
+		Vector beforeY(0,0,0);
+		//Division number along diameter
+		int RDIV=12;
+		if(Radius*2.*pi/12.<baseRes)
 		{
-				std::map<Delaunay::Cell_handle,int>::iterator it_c=index.find(c);
-				int d=it_c->second;
-				cells[d]+=itr->second;
-				before=c;
-				S1++;
-		}
-		if(((int)N/NN)*NN==N)
-		{
-			std::cout<<"*";
+			RDIV=12;
+		}else{
+			RDIV=(int)(Radius*2.*pi/baseRes);
 		}
 
+		//Generate base particles
+		vector<Vector> vectors;
+		vector<Point> particles;
+		double alpha=pi/((double)RDIV);
+		double R2=Radius*std::cos(alpha);
+		for (int i=0;i<RDIV;i++)
+		{
+			double theta=2.*pi*((double)i)/((double)RDIV);
+			Vector vector(R2*std::cos(theta),R2*std::sin(theta),0);
+			vectors.push_back(vector);
+		}
+		double edge=2*std::sin(alpha)*Radius;
+		double dx=edge/YDIV;
+		for(double i=-Radius;i<Radius;i+=dx)
+		{
+			for(double j=-Radius;j<Radius;j+=dx)
+			{
+				Vector V(i,j,0);
+				bool flag=true;
+				for(auto T=vectors.begin();T!=vectors.end();T++)
+				{
+					if ((*T)*V<R2*R2)
+					{
+						continue;
+					}else
+					{
+						flag=false;
+						break;
+					}
+            
+				}
+				if(flag)
+					particles.push_back(Point(i,j,0));
+			}
+		}
+
+		while(itrC!=_branch->end()-1)
+		{
+			//Division number along line
+			Point P=*itrC;
+			Point Q=*(itrC+1);
+			Vector V=Q-P;
+			double Length=std::sqrt(V.squared_length());
+			V=V/Length;
+			int DIV=1;//default division number
+			if(Length<baseRes)
+			{
+				DIV=1;
+			}else{
+				DIV=(int)(Length/baseRes)+1;
+			}
+			DIV*=5;
+
+			for(double ss=0.5;ss<DIV;ss++)
+			{					
+				for(auto particle=particles.begin();particle!=particles.end();particle++)
+				{
+						N++;
+				}
+			}
+			itrC++;
+			itrD++;
+		}
+		itrA++;
+		itrB++;
+	}
+	std::cout<<"interior.size():"<<N<<endl;
+	int size=N;
+	int next=1000000;
+	N=0;
+	itrA=data.begin();
+	itrB=eclipseTree.begin();
+	while(itrA!=data.end())
+	{
+		int YDIV=8;
+		double Radius;
+		br* _branch;
+		eclipses* _eclipses=*itrB;
+		boost::tie(Radius,_branch)=*itrA;
+		br::iterator itrC=_branch->begin();
+		eclipses::iterator itrD=_eclipses->begin();
+		Vector beforeX(0,0,0);
+		Vector beforeY(0,0,0);
+		//Division number along diameter
+		int RDIV=12;
+		if(Radius*2.*pi/12.<baseRes)
+		{
+			RDIV=12;
+		}else{
+			RDIV=(int)(Radius*2.*pi/baseRes);
+		}
+
+		//Generate base particles
+		vector<Vector> vectors;
+		vector<Point> particles;
+		double alpha=pi/((double)RDIV);
+		double R2=Radius*std::cos(alpha);
+		for (int i=0;i<RDIV;i++)
+		{
+			double theta=2.*pi*((double)i)/((double)RDIV);
+			Vector vector(R2*std::cos(theta),R2*std::sin(theta),0);
+			vectors.push_back(vector);
+		}
+		double edge=2*std::sin(alpha)*Radius;
+		double dx=edge/YDIV;
+		for(double i=-Radius;i<Radius;i+=dx)
+		{
+			for(double j=-Radius;j<Radius;j+=dx)
+			{
+				Vector V(i,j,0);
+				bool flag=true;
+				for(auto T=vectors.begin();T!=vectors.end();T++)
+				{
+					if ((*T)*V<R2*R2)
+					{
+						continue;
+					}else
+					{
+						flag=false;
+						break;
+					}
+            
+				}
+				if(flag)
+					particles.push_back(Point(i,j,0));
+			}
+		}
+
+		while(itrC!=_branch->end()-1)
+		{
+			exterior.push_back(*itrC);
+			//Division number along line
+			Point P=*itrC;
+			Point Q=*(itrC+1);
+			Vector V=Q-P;
+			double Length=std::sqrt(V.squared_length());
+			V=V/Length;
+			int DIV=1;//default division number
+			if(Length<baseRes)
+			{
+				DIV=1;
+			}else{
+				DIV=(int)(Length/baseRes)+1;
+			}
+			DIV*=5;
+			//if in the first run, before is igonored
+			//from the second run, before is used
+
+			if(itrC==_branch->begin())
+			{
+				Vector Z(0,0,1);
+				double t=V*Z;
+				if(std::fabs(t)>0.9)
+				{
+					Z=Vector(0,1,0);
+				}
+				beforeX=CGAL::cross_product(V,Z);
+				beforeY=CGAL::cross_product(V,beforeX);
+
+			}else
+			{				
+				//project beforeX and beforeY to the plane at the node
+				double cx=-(beforeX*itrD->Normal)/(V*itrD->Normal);
+				double cy=-(beforeY*itrD->Normal)/(V*itrD->Normal);
+				Vector tmpX=beforeX+cx*V;
+				Vector tmpY=beforeY+cy*V;
+
+				//Compute plane	
+				Vector Z(0,0,1);
+				double t=V*Z;
+				if(std::fabs(t)>0.9)
+				{
+					Z=Vector(0,1,0);
+				}
+				Vector X=CGAL::cross_product(V,Z);
+				Vector Y=CGAL::cross_product(V,beforeX);
+				Vector N=CGAL::cross_product(X,Y);
+				//project tmpX and tmpY to the plane conformed of X and Y
+				beforeX=tmpX-(tmpX*N)*N;
+				beforeY=CGAL::cross_product(V,beforeX);
+
+			}
+			for(double ss=0.5;ss<DIV;ss++)
+			{
+				double s=((double)ss)/((double)DIV);
+				for(auto particle=particles.begin();particle!=particles.end();particle++)
+				{
+
+					Vector B=beforeX*(*particle).x()+beforeY*(*particle).y();
+
+					//Project N
+					double c1=-(B*itrD->Normal)/(V*itrD->Normal);
+					double c2=-(B*(itrD+1)->Normal)/(V*(itrD+1)->Normal);
+					Vector tmp1=B+c1*V;
+					Vector tmp2=B+c2*V;
+					Point D1=(*itrC)+tmp1;
+					Point D2=(*(itrC+1))+tmp2;
+					Point D(D2.x()*s+D1.x()*(1-s),D2.y()*s+D1.y()*(1-s),D2.z()*s+D1.z()*(1-s));
+					if(N==0)
+						c = T.locate(D, lt,li,lj);
+					else
+						c=T.locate(D,lt,li,lj,before);
+						
+					if(lt==Locate_type::CELL)
+					{
+							std::map<Delaunay::Cell_handle,int>::iterator it_c=index.find(c);
+							int d=it_c->second;
+							cells[d]++;
+							before=c;
+					}
+					if(N>=next)
+					{
+						std::cout<<N<<"/"<<size<<endl;
+						next+=1000000;
+					}
+					N++;
+				}
+			}
+
+			itrC++;
+			itrD++;
+		}
+		itrA++;
+		itrB++;
 	}
 	std::cout<<endl;
-	std::cout<<"normal="<<S1<<","<<"error="<<S2<<endl;
-	std::cout<<"end cell search"<<endl;
-	*/
-	/*
+	
+	
+	
 	N=0;
 	for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
 	{
@@ -477,27 +634,28 @@ int main()
 		Vector H=PD-PB;
 		Vector I=PD-PC;
 		double V=std::fabs(CGAL::cross_product(G,H)*I)/6.;
-		density[N]=cells[N]/V;
+		density[N]=V;//cells[N]/V;
 	}
-	*/
+	
 	std::cout<<"T.number_of_finite_cells:"<<T.number_of_finite_cells()<<endl;
 	std::cout<<"T.number_of_finite_facets:"<<T.number_of_finite_facets()<<endl;
 	bool* bool_list=new bool[T.number_of_finite_cells()];
+	bool* bool_list2=new bool[T.number_of_finite_cells()];
 	N=0;
 	double D=0.4;
 	int count=0;
 	for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
 	{
 		bool_list[N]=true;
-//		if(density[N]<D)bool_list[N]=false;
+		if(cells[N]<1)bool_list[N]=false;
+		bool_list2[N]=true;
 	}
-/*	N=0;
-	for(int i=0;i<30;i++)
+	for(int i=0;i<200;i++)
 	{
-		int N=0;
+		N=0;
 		for(Delaunay::Finite_cells_iterator itr=T.finite_cells_begin();itr!=T.finite_cells_end();itr++,N++)
 		{
-			if(bool_list[N])
+			if(bool_list2[N]==true)
 			{
 				bool flag=false;
 				for(int i=0;i<4;i++)
@@ -506,21 +664,24 @@ int main()
 					std::map<Delaunay::Cell_handle,int>::iterator it_N=index.find(_neighbor);
 					if(it_N==index.end())
 					{
-						flag=true;//boundary
+						flag=true;break;
 					}else
 					{
-						if(bool_list[it_N->second]==false)
-							flag=true; //boundary
+						if(bool_list2[N]&&(!bool_list2[it_N->second]))
+						{
+							flag=true;break;
+						}
 					}
 				}
 				if(flag)
 				{
-					if(density[N]<D)bool_list[N]=false;
+					if(bool_list[N]==false)bool_list2[N]=false;
 				}
 			}
 		}
+		std::cout<<"*";
 	}
-	*/
+	std::cout<<endl;
 	N=0;
 
 
@@ -536,12 +697,15 @@ int main()
 			std::map<Delaunay::Cell_handle,int>::iterator it_N=index.find(_neighbor);
 			if(it_N==index.end())
 			{
-				if(bool_list[N])
+				if(bool_list2[N])
 					facet_list.push_back(Delaunay::Facet(itr,i));
 			}else
 			{
-				if(bool_list[N]&&(!bool_list[it_N->second]))
+				if(bool_list2[N]&&(!bool_list2[it_N->second]))
+				{
+					//if(density[it_N->second]>0.00001)
 					facet_list.push_back(Delaunay::Facet(itr,i));
+				}
 			}
 		}
 		if(((int)N/NN)*NN==N)std::cout<<"*";
@@ -550,6 +714,7 @@ int main()
 	std::cout<<"complex created"<<endl;
 	std::cout<<"T.number_of_finite_facets:"<<T.number_of_finite_facets()<<endl;
 	delete(bool_list);
+	delete(bool_list2);
 	
 	//File write
 
